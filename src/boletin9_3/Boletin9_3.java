abe27657157e207dd56329042c0d7d6ba2ed74df
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2015-11-13 12:23:19 +0100

    Boletin9_3

---
+++ src/boletin9_3/Boletin9_3.java
@@ -0,0 +1,15 @@
+
+package boletin9_3;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin9_3 {
+
+    public static void main(String[] args) {
+   Area obx = new Area();
+   obx.CalcularArea();
+    }
+    
+}
